3ba51d847c5329afeb480501df000d2d1b50abdb
Angel Ricardo Nieto García <angelricardoh@gmail.com>
2015-08-08 00:07:14 -0500

    First Program in Swift 2.0

---
+++ TheSwiftProgrammingLanguage2.0/FirstProgram/FirstProgram/main.swift
@@ -0,0 +1,61 @@
+//
+//  main.swift
+//  FirstProgram
+//
+//  Created by Angel Ricardo on 07/08/15.
+//  Copyright (c) 2015 Angel Ricardo. All rights reserved.
+//
+
+import Foundation
+
+let explicitDouble: Double = 70
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+print(appleSummary)
+print(fruitSummary)
+
+var shoppingList = ["catfish", "water", "tulips", "blue paint"]
+shoppingList[1] = "bottle of water"
+
+var occupations = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic",
+]
+
+occupations["Jayne"] = "Pulic Relations"
+
+print(shoppingList)
+print(occupations)
+
+let individualScores = [75, 43, 103, 87, 12]
+
+var teamScore = 0
+
+for score in individualScores {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+
+print(teamScore)
+
+var optionalString: String? = "Hello"
+print(optionalString == nil)
+
+var optionalName: String? = "John Appleseed"
+var greeting = "Hello!"
+
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+}
