ebe2cd217d553cf1505ecdac12c3c82562726da9
Angel Nieto <angel.nieto@globant.com>
2015-09-29 17:18:23 -0500

    Small changes to Control Fun

--- TheSwiftProgrammingLanguage2.0/Control Fun/Control Fun/ViewController.swift
+++ TheSwiftProgrammingLanguage2.0/Control Fun/Control Fun/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var numberField: UITextField!
     @IBOutlet weak var sliderLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,11 +23,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func textFieldDoneEditing(sender: UITextField) {
+        sender.resignFirstResponder()
+    }
 
     @IBAction func sliderChanged(sender: UISlider) {
         let progress = lroundf(sender.value)
         sliderLabel.text = "\(progress)"
     }
 
+    @IBAction func backgroundTap(sender: UIControl) {
+        nameField.resignFirstResponder()
+        numberField.resignFirstResponder()
+    }
 }
 
